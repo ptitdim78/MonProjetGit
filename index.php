0e9d52726c29e3f10da6f3bffe29cdca303a04f6
Dimitri Bourlier <dimitri.bourlier@gmail.com>
2020-11-19 10:20:27 +0100

    premiere version du projet GIT

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon projet Git</title>
+</head>
+<body>
+    <h1 class="mx-auto">Projet Git</h1>
+
+    <div class="container">
+        <p>Ceci est mon projet Git</p>
+    
+    </div>
+</body>
+</html>
